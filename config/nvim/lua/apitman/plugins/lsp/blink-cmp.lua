92b8b219173976824db1fc7bff3d5686668f7d32
Alex Pitman <apitman.dev@gmail.com>
2026-01-04 08:58:56 -0700

    update completions

--- config/nvim/lua/apitman/plugins/lsp/blink-cmp.lua
+++ config/nvim/lua/apitman/plugins/lsp/blink-cmp.lua
@@ -1,26 +1,40 @@
 return {
   {
-    'folke/lazydev.nvim',
-    ft = 'lua',
+    "folke/lazydev.nvim",
+    ft = "lua",
     opts = {
       library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     dependencies = {
-      'rafamadriz/friendly-snippets',
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = {
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
+        },
+      },
     },
-    version = '1.*',
+    version = "1.*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       sources = {
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
         providers = {
           lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
             score_offset = 100,
           },
         },

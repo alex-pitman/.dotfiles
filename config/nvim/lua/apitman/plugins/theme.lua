82cbedaead0a74126f25bc8699f4c17383c9d807
Alex Pitman <apitman.dev@gmail.com>
2026-01-18 08:30:25 -0700

    theme updates for tmux

--- config/nvim/lua/apitman/plugins/theme.lua
+++ config/nvim/lua/apitman/plugins/theme.lua
@@ -1,25 +1,25 @@
 return {
-  'folke/tokyonight.nvim',
+  "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
-    local transparent = false
+    local transparent = true
 
-    local bg = '#011628'
-    local bg_dark = '#011423'
-    local bg_highlight = '#143652'
-    local bg_search = '#0A64AC'
-    local bg_visual = '#275378'
-    local fg = '#CBE0F0'
-    local fg_dark = '#B4D0E9'
-    local fg_gutter = '#627E97'
-    local border = '#547998'
+    local bg = "#011628"
+    local bg_dark = "#011423"
+    local bg_highlight = "#143652"
+    local bg_search = "#0A64AC"
+    local bg_visual = "#275378"
+    local fg = "#CBE0F0"
+    local fg_dark = "#B4D0E9"
+    local fg_gutter = "#627E97"
+    local border = "#547998"
 
-    require('tokyonight').setup {
+    require("tokyonight").setup({
       transparent = transparent,
       styles = {
         comments = { italic = false }, -- Disable italics in comments
-        sidebars = transparent and 'transparent' or 'dark',
-        floats = transparent and 'transparent' or 'dark',
+        sidebars = transparent and "transparent" or "dark",
+        floats = transparent and "transparent" or "dark",
       },
       on_colors = function(colors)
         colors.bg = bg
@@ -38,8 +38,8 @@ return {
         colors.fg_gutter = fg_gutter
         colors.fg_sidebar = fg_dark
       end,
-    }
+    })
 
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme("tokyonight-night")
   end,
 }

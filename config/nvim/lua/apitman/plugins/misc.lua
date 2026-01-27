d6dbe294161024bbdbc8a977e2217226a631c8c3
alexander.pitman <alexander.pitman@care.com>
2026-01-27 09:25:09 -0700

    migrate smaller plugins to misc

--- config/nvim/lua/apitman/plugins/misc.lua
+++ config/nvim/lua/apitman/plugins/misc.lua
@@ -1,6 +1,27 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  config = function()
-    require("colorizer").setup()
-  end,
+  {
+    -- Highlight todo, notes, etc.
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+  },
+  {
+    -- Indentation style detection
+    "NMAC427/guess-indent.nvim",
+    opts = {},
+  },
+  {
+    -- Color highligher
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
+    -- Autoclose parentheses, brackets, quotes, etc.
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
 }
